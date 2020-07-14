6e0f259e6b126c6c549d46120c00d3f8d7e1a4ce
Jacob Ashworth <jeashwor@gmail.com>
2020-07-13 23:02:00 -0400

    cleaned up format of css and js file.

--- scheduler.js
+++ scheduler.js
@@ -1,4 +1,3 @@
-// Add Document.ready function to make sure all scripts and page have loaded prior to running this file. 
 // Create Global Variables for access to HTML file.
 var currentDayEl = $("#currentDay");
 var containerEl = $(".container");
@@ -10,7 +9,7 @@ var i = 0;
 
 // Creates variables using moment().
 var today = moment().format("dddd, MMMM Do");
-var hours24 = (moment().format("H") -10);
+var hours24 = (moment().format("H") - 10);
 
 // Get stored ToDoItems if they exist in local storage
 var storedToDoItems = JSON.parse(localStorage.getItem("ToDoItems"));
@@ -25,36 +24,39 @@ if (storedToDoItems !== null) {
 // Display Current Day on page.
 currentDayEl.text(today);
 
-
 // Begin Creating rows with times by hour , form field, and save button. 
 for (var time = 8; time < 18; time++) {
     var i = time - 8;
+    // create div for rows.
     var timeSlot = $("<div>");
     timeSlot.addClass("row time-block");
     timeSlot.attr("time-slot", (time - 8));
+    // create dive for time display.
     var timeBlock = $("<div>");
     timeBlock.addClass("col-md-2 hour");
     setTimeDisplay(time);
     timeBlock.text(toDoHour);
+    // Create text area for input field to be saved. 
     var description = $("<textarea>");
     description.addClass("col-md-9 description")
     description.attr("id", i);
     description.attr("type", "text");
     description.text(toDoItems[i]);
+    // Create save icon area.
     var saveBlock = $("<div>");
     saveBlock.addClass("col-md-1 saveBtn");
     var saveSymbol = $("<a>");
     saveSymbol.addClass("far fa-save");
     saveSymbol.attr("save-id", i);
     saveBlock.append(saveSymbol);
-
+    // call function to set row color.
     rowColor(time, hours24);
-
+    // Append added divs to timeSlot.
     timeSlot.append(timeBlock, description, saveBlock);
     timeSlot.append(description);
     timeSlot.append(saveBlock);
+    // Append timeSlot elements to ContainerEl.
     containerEl.append(timeSlot);
-
 };
 
 // Function to set AM vs PM on calendar hour.  
@@ -78,7 +80,7 @@ function rowColor() {
 };
 
 // Add click event to save element to local storage. 
-$("a").on("click", function(event) {
+$("a").on("click", function (event) {
     event.preventDefault();
     var j = $(this).attr("save-id");
     var descriptionToSave = $("#" + j).val();
